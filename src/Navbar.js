e13fa8bfa129f71b384604ee9bb7a612e66aa616
philippark <philippark271@gmail.com>
2024-04-05 18:21:46 -0400

    styled navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,10 @@
 const Navbar = () => {
     return ( 
         <nav className="navbar">
-        <h1>VocalCards</h1>
         <div className="links">
             <a href="/">Home</a>
             <a href="/create" style={{ 
-            }}>New Blog</a>
-            <button>+</button>
+            }}>Create</a>
         </div>
         </nav>
      );
