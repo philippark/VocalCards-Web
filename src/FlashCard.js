27c10d69c70d1647c4c781ddd41011151fca19a8
philippark <philippark271@gmail.com>
2024-04-21 21:31:43 -0400

    added basic flashcard content

--- src/FlashCard.js
+++ src/FlashCard.js
@@ -8,13 +8,30 @@ const FlashCard = () => {
 
     const { error, isPending, data: deck } = useFetch('http://localhost:8000/decks/' + id)
 
+    const [showAnswer, setShowAnswer] = new useState(false);
+
+    const handleClick = () => {
+        setShowAnswer(!showAnswer);
+    }
+
     return (
         <div className = "card-content">
             {isPending && <h2>Loading...</h2>}
             { error && <div>{ error }</div> }
             {deck && 
-            <div>
-                <h2>{deck.flashcards[0][0]}</h2>
+            <div className = "flashcard-content">
+                <p>{deck.flashcards[0][0]}</p>
+
+                {showAnswer ?
+                <div className = "post-answer">
+                    <button onClick={handleClick}>Again</button>
+                    <button onClick={handleClick}>Pass</button>
+                </div>
+                :
+                <div className = "pre-answer">
+                    <button onClick={handleClick}>Show Answer</button>
+                </div>
+                }
             </div>
             }
         </div>
