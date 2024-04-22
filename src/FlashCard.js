ba4b573084c1f5c95f73924e38ea79b7e0dcd04a
philippark <philippark271@gmail.com>
2024-04-21 21:43:38 -0400

    added incrementing flashcards, need better structure

--- src/FlashCard.js
+++ src/FlashCard.js
@@ -9,9 +9,16 @@ const FlashCard = () => {
     const { error, isPending, data: deck } = useFetch('http://localhost:8000/decks/' + id)
 
     const [showAnswer, setShowAnswer] = new useState(false);
+    const [flashcardIndex, setFlashcardIndex] = new useState(0);
 
     const handleClick = () => {
         setShowAnswer(!showAnswer);
+
+        if (showAnswer) {
+            let i = flashcardIndex;
+            i+=1;
+            setFlashcardIndex(i);
+        }
     }
 
     return (
@@ -20,15 +27,18 @@ const FlashCard = () => {
             { error && <div>{ error }</div> }
             {deck && 
             <div className = "flashcard-content">
-                <p>{deck.flashcards[0][0]}</p>
-
                 {showAnswer ?
                 <div className = "post-answer">
+
+                    <p>{deck.flashcards[flashcardIndex][1]}</p>
+
                     <button onClick={handleClick}>Again</button>
                     <button onClick={handleClick}>Pass</button>
                 </div>
                 :
                 <div className = "pre-answer">
+                    <p>{deck.flashcards[flashcardIndex][0]}</p>
+
                     <button onClick={handleClick}>Show Answer</button>
                 </div>
                 }
