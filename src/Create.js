f13752d574b07bb761f88006158780dd96532439
philippark <philippark271@gmail.com>
2024-04-25 21:57:59 -0400

    deck creation, bug: not refreshing page

--- src/Create.js
+++ src/Create.js
@@ -1,13 +1,33 @@
 import Popup from "reactjs-popup";
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
     const [name, setName] = useState("");
+    const flashcards = [];
+
+    const history = useHistory();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const deck = { name, flashcards};
+    
+        fetch('http://localhost:8000/decks/', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(deck)
+        }).then(() => {
+            history.push('/');
+        })
+    }
+
     return ( 
         <div className="create">
-            <input type = "text" required value = {name} onChange = {(e)=>setName(e.target.value)}/>
-            <button>Ok</button>
-            <button>Cancel</button>
+            <form onSubmit={handleSubmit}>
+                <input type = "text" required value = {name} onChange = {(e)=>setName(e.target.value)}/>
+                <button>Ok</button>
+                <button>Cancel</button>
+            </form>
         </div>
      );
 }
