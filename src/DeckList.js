de9914bc9e685c7e588ce6c5cc9c5945085c868b
philippark <philippark271@gmail.com>
2024-05-07 12:55:36 -0400

    init edit and delete button:

--- src/DeckList.js
+++ src/DeckList.js
@@ -1,35 +1,40 @@
-import { Link } from 'react-router-dom';
+import { useHistory, Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 
 const DeckList = ({decks}) => {
+    const history = useHistory();
+
+    const handleDelete = (id) => {
+        fetch("http://localhost:8000/decks/" + id,{
+            method: "DELETE"
+        }).then(()=>{
+            window.location = "/";
+        })
+    }
+
     return ( 
         <div className = "deck-list">
-            
-            {/*
+
             {decks.map(deck=>(
-                
+                /*
                 <Link to={`/decks/${deck.id}`}>
                     <div className = "deck"  >
-                        <h2 onContextMenu={(e)=>handleOnContextMenu(e, deck)} >{deck.name}</h2>
+                        <h2>{deck.name}</h2>
+                        <button onClick={handleDelete}>Delete</button>
                     </div>
                 </Link>
+                */
+                
+                <div className = "deck"  >
+                    <h2>{deck.name}</h2>
+                    <button>Edit</button>
+                    <button onClick={()=> handleDelete(deck.id)}>Delete</button>
+                    
+                </div>
+                
             )
             )   
             }
-            */}
-
-            {
-            <ul>
-                {decks.map(deck=>{
-                    return (
-                        <li key = {deck.id}>
-                        {deck.name}
-                        </li>
-                    )
-                })}
-            </ul>
-            }
-            
 
         </div>
      );
